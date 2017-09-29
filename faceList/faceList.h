b630669178e5cd2115fd5a6556b5746ada8c0e2b
rca_fernando_vasques <fernando.vasques@rcadigital.com.br>
2017-09-29 15:37:52 -0300

    atividata co lista inicio

---
+++ faceList/faceList.h
@@ -0,0 +1,48 @@
+typedef struct 
+{
+	char *nome,
+	int idade,
+	char *estadoCivil
+} pessoa;
+typedef struct
+{
+	pessoa info,
+	typedef struct
+	{
+		struct perfil *lista,
+		int tam
+	}seguindo,
+	typedef struct
+	{
+		struct perfil *lista,
+		int tam
+	} seguidores
+
+} perfil;
+
+typedef struct
+{
+	perfil *perfis,
+	int tam
+} usuarios;
+
+void listarUsuarios(usuarios *u);
+
+void listarSeguidores(perfil *p);
+
+void listarSeguindo(perfil *p);
+
+perfil addUsuario(char *nome, int idade, char *eC);
+
+void addPerfil(usuarios *u, perfil *p);
+
+void seguir(perfil *p);
+
+void desSeguir(perfil *p);
+
+perfil getUsuario(int i);
+
+
+
+
+
---
+++ faceList/main.c
@@ -0,0 +1,14 @@
+#include <sdtio.h>
+#include <stdlib.h>
+#include "faceList.h"
+
+void menu();
+int main()
+{
+
+	// TENHO QUE IMPLEMENTAR E BIBLIOTECA
+	return 0;
+}
+void menu(){
+
+}
