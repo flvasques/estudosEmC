98a62a5d5759599025037ccfebc5c091db789e7e
flvasques <fflqvn@gmail.com>
2017-11-04 16:28:08 -0200

    clean

--- lista contigua/listaContigua.h
+++
@@ -1,9 +0,0 @@
-typedef struct{int elemento;} no;
-typedef struct{no *vetor; int tamanho;}listaVetor;
-no *novoNo(int i);
-listaVetor *novaLista();
-void novoElemento(no *n, listaVetor *lista);
-void elementoPosicao(no *n,int pos, listaVetor *lista);
-void apagarElementoX(int pos, listaVetor *lista);
-int verificaElemento(no *n,listaVetor *lista);
-int qtdElementos(listaVetor *lista);
